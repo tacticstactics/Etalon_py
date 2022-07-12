c68809d61c51a71a78df43deead5cad28fe7b053
S <30459885+tacticstactics@users.noreply.github.com>
2022-07-12 10:34:43 +0900

    1

--- Etalon_py/Etalon_py/Etalon_def.py
+++ Etalon_py/Etalon_py/Etalon_def.py
@@ -37,31 +37,17 @@ def proc1(param=0.01,m=256):
 
         wl = wl0 + ii * stepwl
 
-        wlcols = 0l[(ii)] 
+        wlcol[(ii)] =wl
+
+        PT = 1
+
+        PTetacol[(ii)] =PT
+
+        PR = 1
+
+        PRetacol[(ii)] = PR
         
-        for jj in range(m):
 
-            time1 = 0.025*jj-1.6
-            tcol[(jj)] = time1
-            
-            Et = np.exp(1j * omega * time1) * np.exp(-1.38 * (time1/tau)**2)
-            
-            Etcol[(jj)] = Et
-            
-            #Et = math.sin(omega * time1)     
-            
-
-            td = time1 + delay1
-
-            Etd = np.exp(1j * omega * td) * np.exp(-1.38 * (td/tau)**2)                          
-     
-
-            s += (abs(Et+Etd)**2)**2            
-
-              #Signalcol[(ii)] = Etd
-            Signalcol[(ii)] = s
-
-                    #print(Signalcol)
 
     return wlcol, PTetacol,PRetacol
 
